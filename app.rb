65cf13819d74803b7c611e7a04c8c8a884723721
Vinceng63 <gueganvincent@gmail.com>
2019-04-15 18:05:13 +0200

    maj

--- app.rb
+++ app.rb
@@ -10,3 +10,17 @@ require_relative 'lib/event'
 
 # Maintenant c'est open bar pour tester ton application. Tous les fichiers importants sont chargés
 # Tu peux faire User.new, Event.new, binding.pry, User.all, etc.
+
+
+vincent = User.new("vincent@gmail.com",34)
+lucas = User.new("lucas@gmail.com",28)
+
+test = Event.new("2019-04-15 15:25", 10, "standup quotidien", ["truc@machin.com", "bidule@chose.fr"])
+# test.start_date
+# #test.postpone_24h
+# test.end_date
+# test.is_past?
+# test.is_future?
+# test.is_soon?
+# test.to_s
+puts user1 = User.find_by_email("vincent@gmail.com")[0].age
--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,44 @@
-class Event
+require "time"
 
+class Event
+  attr_accessor :start_date, :duration, :title, :attendees
+
+  def initialize(start_date, duration, title, attendees)
+    @start_date = Time.parse(start_date)
+    @duration = duration.to_i
+    @title = title
+    @attendees = attendees.to_a
+  end
+
+  def postpone_24h
+    start_date += 3600 * 24
+  end
+
+  def end_date
+    puts @start_date + @duration * 60
+  end
+
+  def is_past?
+    return @start_date < Time.now
+  end
+
+  def is_future?
+    return !is_past?
+  end
+
+  def is_soon?
+      if is_future?
+        return @start_date-30*60 < Time.now
+      else
+        return is_future?
+      end
+  end
+
+  def to_s
+      puts "Titre : #{@title}"
+      puts "Date de début : #{@start_date}"
+      puts "Durée : #{@duration} minutes"
+      puts "Invités : " + @attendees.join(", ")
+  end
+  
 end
