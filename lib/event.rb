d66f3c00a5c9999b1034f0acb4ac811a4c47e728
Vinceng63 <gueganvincent@gmail.com>
2019-04-15 20:07:22 +0200

    maj correction

--- lib/event.rb
+++ lib/event.rb
@@ -1,21 +1,19 @@
-require "time"
-
 class Event
   attr_accessor :start_date, :duration, :title, :attendees
 
   def initialize(start_date, duration, title, attendees)
     @start_date = Time.parse(start_date)
-    @duration = duration.to_i
+    @duration = duration
     @title = title
-    @attendees = attendees.to_a
+    @attendees = attendees
   end
 
   def postpone_24h
-    start_date += 3600 * 24
+    @start_date += 60*60*24
   end
 
   def end_date
-    puts @start_date + @duration * 60
+    return @start_date + duration*60
   end
 
   def is_past?
@@ -23,22 +21,19 @@ class Event
   end
 
   def is_future?
-    return !is_past?
+    !self.is_past?
   end
 
   def is_soon?
-      if is_future?
-        return @start_date-30*60 < Time.now
-      else
-        return is_future?
-      end
+    return @start_date < Time.now + 60*30
   end
 
   def to_s
-      puts "Titre : #{@title}"
-      puts "Date de début : #{@start_date}"
-      puts "Durée : #{@duration} minutes"
-      puts "Invités : " + @attendees.join(", ")
+    puts ">Titre : #{@title}"
+    puts ">Date de début : #{@start_date}"
+    puts ">Durée : #{@duration} minutes"
+    print ">Invités :"
+    puts attendees.join(", ")
   end
-  
+
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,19 +1,28 @@
 class User
-  attr_accessor :email, :age
+  attr_accessor :age, :email
   @@all_users = []
 
-  def initialize(email_to_save, age_to_save)
-    @email = email_to_save
-    @age = age_to_save
+  def initialize(email, age)
+    @email = email
+    @age = age
     @@all_users << self
   end
 
   def self.all
-    return @@all_users
+    @@all_users
   end
-  
-  def self.find_by_email(input)
-    return @@all_users.select{|user| user.email==input}
+
+  def self.find_by_email(email)
+    @@all_users.each do |user|
+
+      if user.email == email
+        return user
+      end
+
+    end
+
+    puts "aucun utilisateur n'a cet email"
+    return false
   end
 
 end
